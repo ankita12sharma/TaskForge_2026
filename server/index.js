36340d737649b150711c4c5cfb081845aa04ef75
Ankita Sharma <your-github-email@example.com>
2026-08-19 22:32:04 +0530

    Show MongoDB connection error

--- server/index.js
+++ server/index.js
@@ -28,6 +28,6 @@ app.use("/", TaskRouter);
 app.use("/", SubTaskRouter);
 app.use("/", commentsRouter);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Listening to server on port ${PORT}`);
 });
